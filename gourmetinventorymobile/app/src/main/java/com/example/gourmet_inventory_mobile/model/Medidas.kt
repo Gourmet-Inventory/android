3ea0f5a04a85f06bcfcaeac555cb53f9b0c1d518
Mirella-Ot <mirella.vidal@sptech.school>
2024-10-07 21:47:42 -0300

    Algumas novas Model

---
+++ gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/model/Medidas.kt
@@ -0,0 +1,14 @@
+package com.example.gourmet_inventory_mobile.model
+
+enum class Medidas {
+    COLHER_DE_SOPA,
+    COLHER_DE_CHA,
+    XICARA,
+    GRAMAS,
+    QUILOGRAMA,
+    MILILITROS,
+    LITRO,
+    A_GOSTO,
+    PITADA,
+    UNIDADE
+}
