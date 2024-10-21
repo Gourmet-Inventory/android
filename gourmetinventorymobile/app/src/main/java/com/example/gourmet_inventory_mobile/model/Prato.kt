18705f931dfc1868fbc98e446399f30bf9a984fd
Luan Ribeiro <luan.magno@sptech.school>
2024-10-21 12:32:53 -0300

    UPDATE - Atualizando Serializeble em models

--- gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/model/Prato.kt
+++ gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/model/Prato.kt
@@ -1,3 +1,8 @@
 package com.example.gourmet_inventory_mobile.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Prato(val nome: String, val preco: Double, val rendimento: String)
+
+data class PratoResponse(val pratos: List<Prato>)
--- gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/model/User.kt
+++ gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/model/User.kt
@@ -1,13 +1,14 @@
 package com.example.gourmet_inventory_mobile.model
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.serialization.Serializable
 
-@Parcelize
+@Serializable
 data class User(
     val email : String,
     val password : String,
-    val role : String
-) : Parcelable
+    val role : String,
+    val name : String,
+    val phone : String,
+)
 
 data class UserResponse(val users: List<User>)
