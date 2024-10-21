5d719ea35fe6188620ee74de4c1c207d6f19b9f1
Luan Ribeiro <luan.magno@sptech.school>
2024-10-21 12:32:53 -0300

    UPDATE - Atualizando Serializeble em models

--- gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/model/Comanda.kt
+++ gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/model/Comanda.kt
@@ -4,10 +4,10 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import kotlinx.serialization.Serializable
 
-@Parcelize
+@Serializable
 data class Comanda(
     val mesa: String,
     val nomeCliente: String,
     val nomeComanda: String,
     val garcom : User,
-) : Parcelable
+)
