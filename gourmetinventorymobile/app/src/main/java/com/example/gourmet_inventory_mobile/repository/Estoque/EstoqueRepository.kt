abe55ea7a42e47b34620421d036c2b947ba1bd5a
Luan Ribeiro <125397916+LuanMagno811@users.noreply.github.com>
2024-11-02 18:52:06 -0300

    Delete gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/repository/Estoque directory

--- gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/repository/Estoque/EstoqueRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gourmet_inventory_mobile.repository.Estoque
-
-import com.example.gourmet_inventory_mobile.model.EstoqueConsulta
-import retrofit2.Response
-
-interface EstoqueRepository {
-    suspend fun obterEstoque(): Response<List<EstoqueConsulta>>
-}
