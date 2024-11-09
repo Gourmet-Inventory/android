8e5cf6c8817de1e9e6af9bd5b65278f9c4bdab1f
Luan Ribeiro <125397916+LuanMagno811@users.noreply.github.com>
2024-11-08 21:27:09 -0300

    Update EstoqueRepository.kt

--- gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/repository/Estoque/EstoqueRepository.kt
+++ gourmetinventorymobile/app/src/main/java/com/example/gourmet_inventory_mobile/repository/Estoque/EstoqueRepository.kt
@@ -1,8 +1,16 @@
-package com.example.gourmet_inventory_mobile.repository.Estoque
+package com.example.gourmet_inventory_mobile.repository.estoque
 
-import com.example.gourmet_inventory_mobile.model.EstoqueConsulta
+import com.example.gourmet_inventory_mobile.model.estoque.Estoque
+import com.example.gourmet_inventory_mobile.model.estoque.EstoqueConsulta
+import com.example.gourmet_inventory_mobile.model.estoque.EstoqueCriacao
 import retrofit2.Response
 
 interface EstoqueRepository {
-    suspend fun obterEstoque(): Response<List<EstoqueConsulta>>
+    suspend fun getEstoque(idEmpresa: Long) : Response<List<EstoqueConsulta>>
+
+    suspend fun createEstoque(idEmpresa: Long ,estoque: EstoqueCriacao) : Response<EstoqueConsulta>
+
+    suspend fun updateEstoque(idEmpresa: Long ,estoque: Estoque) : Response<EstoqueConsulta>
+
+    suspend fun deleteEstoque(id: Long) : Response<Unit>
 }
